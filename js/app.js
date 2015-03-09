b9e8e75f1bab327c9a12e971787f1815beacad06
josephbrockw <joew@theroasterie.com>
2015-03-09 15:45:43 -0500

    A little styling, no js progress

--- js/app.js
+++ js/app.js
@@ -3,14 +3,19 @@ $('Document').ready(function() {
 // Add list item to 'To Buy'
 	$( ".addbutton" ).mouseup(function () {
 //  Add Validation Alert
-			$(".toBuylist").append('<li><div id="textcontain"><p>' + $(".add-bar").val() + '</p></div><button type="button">Delete</button></li>');
+			$(".toBuylist").append('<li><div class="textcontain"><p>'+ $(".add-bar").val() + '</p></div><button class="deletebutton" type="button">Delete</button></li>');
 			$(".add-bar").val("");
 	});
+
+
+	
 // Delete Item
-	$(".toBuylist li button").mouseup(function () {
+	$(".deletebutton").mouseup(function () {
 		alert('on the right track');
 	})
 
 
 
 })
+
+ 
