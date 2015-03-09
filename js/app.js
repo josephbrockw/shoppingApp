508253331586c97365b3747ad9b8ef76ea2047d1
josephbrockw <joew@theroasterie.com>
2015-03-09 10:51:45 -0500

    add-bar working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 $('Document').ready(function() {
-	$( ".addbutton" ).mousedown(function () {
-		alert($('.add-bar').val());
+
+// Add list item to 'To Buy'
+	$( ".addbutton" ).mouseup(function () {
+//  Add Validation Alert
+			$(".toBuylist").append('<li><div id="textcontain"><p>' + $(".add-bar").val() + '</p></div><button type="button">Delete</button></li>');
+			$(".add-bar").val("");
 	});
+// Delete Item
+	$(".toBuylist li button").mouseup(function () {
+		alert('on the right track');
+	})
+
+
+
 })
